fd1964783f62e780a49a988856fc0eaa97a723ab
Lucas Fonseca Mundim <mundim@Lucass-MacBook-Pro.local>
2019-03-25 18:05:16 -0300

    Add feature to clone or use already cloned repo; add text prints to make responses more intuitive

--- Exercicio1/main.py
+++ Exercicio1/main.py
@@ -22,7 +22,7 @@ def GetTotalCommits(repo, yearFrom="1969", yearTo="2020", message=""):
 			break
 	print ('Total number of commits: {}'.format(count))
 
-def GetTopCommiters(repo, yearFrom="1969", yearTo="2020"):
+def GetTopCommiters(repo, yearFrom="1969", yearTo="2020", max=10):
 	allCommits = repo.iter_commits()
 	dictionary = {}
 	count = 0
@@ -37,10 +37,11 @@ def GetTopCommiters(repo, yearFrom="1969", yearTo="2020"):
 					dictionary[element.author.name] = 1
 		except StopIteration:
 			break
+	print('Top %d Committers (from %s to %s):' % (max, yearFrom, yearTo))
 	for key, value in sorted(dictionary.items(), key=lambda kv: kv[1], reverse=True):
 		print (key, value)
 		count = count + 1
-		if count == 9:
+		if count == max:
 			break
 
 def GetNumberOfEdits(repo, add=True, searchString='List<'):
@@ -80,6 +81,7 @@ def GetMostModifiedFiles(repo, yearFrom="1969", yearTo="2020", extension=".cs", 
 							dictionary[key] = 1
 		except StopIteration:
 			break
+	print("Top %d modified files with extension %s (from %s to %s):" % (max, extension, yearFrom, yearTo))
 	for key, value in sorted(dictionary.items(), key=lambda kv: kv[1], reverse=True):
 		print (key, value)
 		count = count + 1
@@ -87,16 +89,23 @@ def GetMostModifiedFiles(repo, yearFrom="1969", yearTo="2020", extension=".cs", 
 			break
 
 parser = argparse.ArgumentParser(description="Arguments Description")
-parser.add_argument('--repo', nargs='?', default='./lime-csharp', help='Repo to use')
+parser.add_argument('--repo', nargs='?', default='https://github.com/takenet/lime-csharp', help='Repo to use')
+parser.add_argument('--folder', nargs='?', default='', help='Folder to use')
 parser.add_argument('--option', nargs='?', default=1, help='Option selected from list')
 parser.add_argument('--yearTo', nargs='?', default="2019", help='Year to check up to')
 parser.add_argument('--yearFrom', nargs='?', default="1969", help='Year to check from')
 parser.add_argument('--searchString', nargs='?', default=1, help='String to search for')
-parser.add_argument('--count', nargs='?', default=5, help='Positions returned')
+parser.add_argument('--count', nargs='?', default=10, help='Positions returned')
 parser.add_argument('--fileType', nargs='?', default='.cs', help='File extension to search for')
 
 args = parser.parse_args()
-repo = Repo(args.repo)
+if args.folder != '':
+	folder = args.folder
+else:	
+	repoName = args.repo.split('/')[-1].split('.')[0]
+	folder = './'+repoName
+	Repo.clone_from(args.repo, folder)
+repo = Repo(folder)
 
 #1. Quantos commits possui o projeto? 
 if args.option == "1":
@@ -108,23 +117,23 @@ elif args.option == "2":
 
 #3. Quantos commits incluem a string “feature”? E a string “fix”? 
 elif args.option == "3":
-	GetTotalCommits(repo, "1969", "2020", args.searchString)
+	GetTotalCommits(repo, message=args.searchString)
 
 #4. Quais os 5 arquivos <extensao> mais modificados? 
 #5. Quais os 5 arquivos <extensao> mais modificados desde 2018? 
 #6. Quais os 5 arquivos <extensao> mais modificados em 2017? 
 elif args.option == "4" or args.option == "5" or args.option == "6":
 	print ("This might take a while if the selected repository has a big number of commits")
-	GetMostModifiedFiles(repo, args.yearFrom, args.yearTo, args.fileType, args.count)
+	GetMostModifiedFiles(repo, args.yearFrom, args.yearTo, args.fileType, int(args.count))
 
 #7. Quais os 3 desenvolvedores mais ativos? 
 # Obs: Serão printados por default os top10, pois o repositório de exemplo tem múltiplos committers que são o mesmo usuário
 elif args.option == "7":
-	GetTopCommiters(repo)
+	GetTopCommiters(repo, max=int(args.count))
 
 #8. Quais 3 desenvolvedores mais ativos em 2019? 
 elif args.option == "8":
-	GetTopCommiters(repo, args.yearFrom, args.yearTo)
+	GetTopCommiters(repo, args.yearFrom, args.yearTo, int(args.count))
 
 #9. Quantas vezes List foi adicionado no código? 
 elif args.option == "9":
